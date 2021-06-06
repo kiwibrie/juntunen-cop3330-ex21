b5f9479b7d446940d435147025b40b1c4b75f5b9
kiwibrie <brianne0128@gmail.com>
2021-06-06 16:23:38 -0500

    Initial commit

---
+++ src/main/java/base/App.java
@@ -0,0 +1,16 @@
+package base;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 21 Solution
+ *  Copyright 2021 Brianne Juntunen
+ */
+import java.util.Scanner;
+
+public class App {
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        App myApp = new App();
+
+    }
+}
