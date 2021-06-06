7db567ffcf756a60a4be4c3233b9f7c89d5fe677
kiwibrie <brianne0128@gmail.com>
2021-06-06 17:25:37 -0500

    working base

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -12,5 +12,38 @@ public class App {
     public static void main(String[] args) {
         App myApp = new App();
 
+        int num = myApp.promptMonth();
+
+        String month = myApp.checkMonthDatabase(num);
+
+        myApp.printOutput(month);
+
+    }
+
+    public void printOutput(String msg){
+        System.out.printf("The name of the month is %s.", msg);
+    }
+
+    public String checkMonthDatabase(int num){
+        switch (num) {
+            case 1: return "January";
+            case 2: return "February";
+            case 3: return "March";
+            case 4: return "April";
+            case 5: return "May";
+            case 6: return "June";
+            case 7: return "July";
+            case 8: return "August";
+            case 9: return "September";
+            case 10: return "October";
+            case 11: return "November";
+            case 12: return "December";
+            default: return "non-existent";
+        }
+    }
+
+    public int promptMonth(){
+        System.out.print("Please enter the number of the month: ");
+        return Integer.parseInt(in.nextLine());
     }
 }
